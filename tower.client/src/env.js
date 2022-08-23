ddc372eb12960ef44d7dac1b4cabc381bf8da33d
Savannah Temple <savtemple@gmail.com>
2022-08-23 16:41:59 -0600

    init

---
+++ tower.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-gmunio0i.us.auth0.com'
+export const audience = 'https://codeworksclassrom.com'
+export const clientId = 'I6v1O4PjfTtnUidIlC7MPEUr8KGKITnl'
